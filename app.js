a892fbbe8b4036ecdf723133a1c7b60472dd4cc4
Taimoor Ali <taimoor0217@gmail.com>
2020-05-21 06:38:36 +0500

    set-up templates and paths

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const data = require('./data.json')
+
 app.use(express.json());
 app.set('views' , path.join(__dirname , 'views'));
 app.set(`view engine` , 'pug')
-
-
+app.use(express.static(__dirname + '/public'));
 
 
 app.get('/ping' , (req , res)=>{
@@ -15,9 +16,23 @@ app.get('/ping' , (req , res)=>{
 
 app.get('/' , (req , res)=>{
   res.render("index" , {
-    title: "My Portfolio"
+    ...data.myInfo,
+    "projects": data.projects
   })
 })
+
+app.get('/projects/:id' , (req , res)=>{
+  // console.log(req.params.id)
+  res.render("project" , {
+    ...data.projects[req.params.id - 1]
+  })
+})
+app.get('/about' , (req , res)=>{
+  res.render("about" ,{
+    ...data.myInfo
+  })
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}!`);
